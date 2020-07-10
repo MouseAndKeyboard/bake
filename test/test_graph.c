8860932e7035abfd5afe808288f17050d90c65d6
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-07-10 14:43:04 +0800

    graph test

---
+++ test/test_graph.c
@@ -0,0 +1,19 @@
+#include "unity.h"
+
+#include <stdlib.h>
+
+#include "graph.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_graph_NeedToImplement(void)
+{
+    node_t *root = malloc(sizeof(node_t));
+    free(root);
+}
