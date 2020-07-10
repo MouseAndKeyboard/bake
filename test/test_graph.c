3b96c122529178c3da1233c1837c72b7507ba8d0
mnef <31727915+MouseAndKeyboard@users.noreply.github.com>
2020-07-10 16:39:34 +0800

    create actions test

--- test/test_graph.c
+++ test/test_graph.c
@@ -17,3 +17,13 @@ void test_create_node(void)
     node_t *root = new_node();
     delete_node(root);
 }
+
+void test_create_actions(void)
+{
+    node_t *root = new_node();
+    add_action(root, "action1");
+    add_action(root, "action number 2");
+    TEST_ASSERT_TRUE( strcmp(root->actions[0], "action1") );
+    TEST_ASSERT_TRUE( strcmp(root->actions[0], "action2") );
+    delete_node(root);
+}
